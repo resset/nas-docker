16bd49833e9845fc84762d4486c07881ec0248d2
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2019-10-07 21:21:37 +0200

    Clone meta-openembedded during setup

--- get_sources.sh
+++ get_sources.sh
@@ -6,5 +6,6 @@ git clone git://git.yoctoproject.org/poky -b warrior
 
 cd poky/
 
+git clone https://github.com/openembedded/meta-openembedded.git -b warrior
 git clone https://github.com/linux-sunxi/meta-sunxi.git -b warrior
 git clone https://github.com/resset/meta-nas.git
