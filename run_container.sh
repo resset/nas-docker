dfeb5934acfd2dd1b1511219676dffd16ff31669
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2019-10-07 22:43:42 +0200

    run: make username generic, yeah...

--- run_container.sh
+++ run_container.sh
@@ -6,5 +6,5 @@ sudo docker run \
     --network=host \
     --hostname=nas-build \
     -v "$(realpath ../poky)":/home/nas/poky \
-    -v /home/pillot/.gitconfig:/home/nas/.gitconfig:ro \
+    -v /home/${USER}/.gitconfig:/home/nas/.gitconfig:ro \
     nas_docker
