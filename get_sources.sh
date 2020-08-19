f7eb0057b73d8641249012d529681b11dca826f2
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2020-08-19 23:59:50 +0200

    Change yocto version to dunfell

--- get_sources.sh
+++ get_sources.sh
@@ -2,10 +2,10 @@
 
 cd ../
 
-git clone git://git.yoctoproject.org/poky -b warrior
+git clone git://git.yoctoproject.org/poky -b dunfell
 
 cd poky/
 
-git clone https://github.com/openembedded/meta-openembedded.git -b warrior
-git clone https://github.com/linux-sunxi/meta-sunxi.git -b warrior
+git clone https://github.com/openembedded/meta-openembedded.git -b dunfell
+git clone https://github.com/linux-sunxi/meta-sunxi.git -b dunfell
 git clone https://github.com/resset/meta-nas.git
